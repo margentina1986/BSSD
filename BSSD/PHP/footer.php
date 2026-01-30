b7eea5ea916fe2e09dc58620179abdceb22e6c10
margentina1986 <margentina1986@gmail.com>
2026-01-30 15:14:40 +0900

    Add files via upload
    「上へ戻る」リンクの追加（主に検索結果ページで多用される想定だが、バージョン違い管理表ページなどでも使われる可能性があるためフッターに追加）

--- BSSD/PHP/footer.php
+++ BSSD/PHP/footer.php
@@ -6,4 +6,5 @@
     <p>Built with PHP, Python, and MySQL.</p>
     <!-- <p><a href="privacy.php">Privacy Policy</a></p>-->
     <p>Copyright © 2026 BSSD. All Rights Reserved.</p>
+    <p><a href="#page-top" class="back-to-top">↑ 上へ戻る</a></p>
 </footer>
