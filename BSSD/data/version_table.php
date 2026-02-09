5efc0965af37cc2d9e68ea19bab234be85ba25ba
margentina1986 <margentina1986@gmail.com>
2026-02-09 10:27:46 +0900

    Add files via upload
    新規作成

---
+++ BSSD/data/version_table.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    [
+        'title' => 'だからその手を離して',
+        'version' => 'オリジナル',
+        'first_work' => "B'z",
+        'difference' => '-',
+        'policy' => 'シングル・アルバム同時発売だが
+        アルバムを初回収録作品として扱う',
+        'note' => '打ち込みのため
+        検索結果には表示されず',
+    ],
+    [
+        'title' => 'だからその手を離して',
+        'version' => 'DA・KA・RA・SO・NO・TE・O・HA・NA・SHI・TE
+         -OFF THE LOCK STYLE-',
+        'first_work' => 'BAD COMMUNICATION',
+        'difference' => '全英詞リメイク',
+        'policy' => '別曲扱い',
+        'note' => '打ち込みのため
+        検索結果には表示されず',
+    ],
+    [
+        'title' => 'だからその手を離して',
+        'version' => 'Mixture Style',
+        'first_work' => '今夜月の見える丘に',
+        'difference' => '再録',
+        'policy' => '別曲扱い',
+        'note' => '打ち込みのため
+        検索結果には表示されず',
+    ],
+];
