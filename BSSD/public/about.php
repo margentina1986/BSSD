8ed310294ee96cad2b042146dedd2683cc8412c9
margentina1986 <margentina1986@gmail.com>
2026-02-12 10:08:42 +0900

    Add files via upload
    拡張子をHTML⇒PHPに変換
    フッター表示の調整

---
+++ BSSD/public/about.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>About This Hp</title>
+    <link rel="stylesheet" href="./styles/common.css">
+    <link rel="stylesheet" href="./styles/about.css">
+    <link rel="stylesheet" href="./styles/footer.css">
+</head>
+
+
+<body>
+    <nav id="menu"></nav>
+
+    <h1>About This HP</h1>
+    <h2>B'zでこんなサイトが欲しかった…</h2>
+
+
+   <p class="content-text">ある日、B'z The “Mixture”を聴いてる時にふと思ったんですよね。<br>
+        <strong>「中村キタローさんと山木秀夫さんのリズム隊って、すごくオシャレで大人で渋くて”The 職人”って感じだなぁ」と。</strong><br><br>
+        それでこのコンビが弾いている曲をピックアップして聴こうと思ったんですが、このコンビって参加している年代の範囲が広いし、色んなアルバムで数曲だけ弾いてることが多いし、調べるのがとても大変でした。<br>「この曲誰が弾いてるのかな？」はWikipedia見ればいいし、個人別の一覧表はそういうサイトを作られている方もいらっしゃるんですけどね。<br>組み合わせが気軽に調べられるサイトはありませんでした。（自分が見つけられないだけの可能性もありますが…）<br><br>
+        そしてその数年後、とある事情でプログラミングを学ぶことになり、<br>簡単なデータベース系のHPなら作成できるようになったので<br><strong>「ほんなら自分で作ったらええやんけ」</strong>となった次第でございます。<br><br>
+        完全な素人がプログラミングをゼロからプログラミングを学び始めて3ヶ月弱でリリースさせたので至らぬ点もあるかと思いますが、日々のサポートメンバー検索に役立てていただければ幸いです。
+
+    </p>
+
+    <script src="scripts/menu.js"></script>
+    <?php include 'parts/footer.php'; ?>
+</body>
+
+</html>
