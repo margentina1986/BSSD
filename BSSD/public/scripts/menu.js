8c0749510e91b395f1142f9bb0d0e29e6fb4ba31
margentina1986 <margentina1986@gmail.com>
2026-02-13 15:12:42 +0900

    Add files via upload
    参照先の修正

--- BSSD/public/scripts/menu.js
+++ BSSD/public/scripts/menu.js
@@ -1,13 +1,13 @@
 // メニュー項目を配列で定義
 const menuItems = [
-    { name: "Top", link: "/top.php" },
-    { name: "About This", link: "/about.php" },
-    { name: "Search", link: "/search.php" },
-    { name: "Recommend", link: "/recommend.php" },
-    { name: "Ranking", link: "/ranking.php" },
-    { name: "Q & A", link: "/qa.php" },
-    { name: "Version", link: "/version.php" },
-    { name: "Contact", link: "/contact.php" }
+    { name: "Top", link: "top.php" },
+    { name: "About This", link: "about.php" },
+    { name: "Search", link: "search.php" },
+    { name: "Recommend", link: "recommend.php" },
+    { name: "Ranking", link: "ranking.php" },
+    { name: "Q & A", link: "qa.php" },
+    { name: "Version", link: "version.php" },
+    { name: "Contact", link: "contact.php" }
 ];
 
 // nav要素を取得
