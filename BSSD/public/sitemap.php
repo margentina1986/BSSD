7c6555da3e379d2c810f7cef33fa9064c9a28e3c
margentina1986 <margentina1986@gmail.com>
2026-02-28 22:17:58 +0900

    Add files via upload
    サイトマップの追加

---
+++ BSSD/public/sitemap.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Map</title>
+    <link rel="stylesheet" href="./styles/common.css">
+    <link rel="stylesheet" href="./styles/sitemap.css">
+    <link rel="stylesheet" href="./styles/footer.css">
+</head>
+
+
+<body id="page-top">
+    <nav id="menu"></nav>
+
+    <h1>Site Map</h1>
+    <h2>各ページの説明です</h2>
+
+    <a href="index.php" class="link">Top</a>
+        <p class="content-text">トップページです。更新履歴も記載しています。</p>
+    <a href="about.php" class="link">About This</a>
+        <p class="content-text">このHPを作った理由やサイトマップへのリンクがあります。</p>
+    <a href="search.php" class="link">Search</a>
+        <p class="content-text">このHPメイン機能。各サポートミュージシャンがどの曲を弾いているか検索ができます。</p>
+    <a href="recommend.php" class="link">Recommend</a>
+        <p class="content-text">生年月日を入力すると、本日のオススメサポートミュージシャンが表示されます。</p>
+    <a href="ranking.php" class="link">Ranking</a>
+        <p class="content-text">参加曲数ランキングと、このHPでの検索回数ランキングが確認できます。</p>
+    <a href="qa.php" class="link">Q & A</a>
+        <p class="content-text">想定質問へ回答しています。新しい問い また聞こえてくる Quetion! Answer!</p>
+    <a href="version.php" class="link">Version</a>
+        <p class="content-text">各楽曲のバージョン違いについて、違いや当HPでの取り扱いかたを記載しています。</p>
+    <a href="contact.php" class="link">Contact</a>
+        <p class="content-text">管理人宛にメッセージや不具合修正依頼を送ることができます。</p>
+
+    <script src="scripts/menu.js"></script>
+    <?php include 'parts/footer.php'; ?>
+</body>
+
+</html>
